cc437cbf354aa8cef74a3c82d3d4a1620a85a12a
evan-rosa <evandanrosa@gmail.com>
2023-10-31 14:04:23 -0400

    refactor getURL

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,15 +1,27 @@
 import logger from './logger';
+import { NextApiRequest } from 'next';
 
-export const getURL = () => {
-  let url =
-    process?.env?.NEXT_PUBLIC_API_URL ?? // Set this to your site URL in production env.
-    'http://localhost:3000/';
-  // Make sure to include `https://` when not localhost.
+export const getURL = (req?: NextApiRequest | null) => {
+  let url;
+
+  // If req is available, it means we're on the server
+  if (req && req.headers) {
+    const proto = req.headers['x-forwarded-proto'] || 'http';
+    url = `${proto}://${req.headers.host}/`;
+  } else {
+    // If we're on the client, use NEXT_PUBLIC_API_URL or fallback to localhost
+    url = 'http://localhost:3000/';
+  }
+
+  // Make sure to include `https://` when not localhost
   url = url.includes('http') ? url : `https://${url}`;
-  // Make sure to including trailing `/`.
+
+  // Make sure to include trailing `/`
   url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+
   return url;
 };
+
 
 export const postData = async ({ url, data }: { url: string; data?: any }) => {
   const res: Response = await fetch(url, {
