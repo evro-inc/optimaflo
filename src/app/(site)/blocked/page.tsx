b1c7dba43500cd9ce28ec3159344dcb4cda5f45e
evan-rosa <evandanrosa@gmail.com>
2024-01-19 18:52:12 -0500

    removed alert from layout

--- src/app/(site)/blocked/page.tsx
+++ src/app/(site)/blocked/page.tsx
@@ -1,13 +1,11 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 
 export default function Blocked() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div>
-        <main>
-          <h3>Access blocked.</h3>
-        </main>
-      </div>
-    </Suspense>
+    <div>
+      <main>
+        <h3>Access blocked.</h3>
+      </main>
+    </div>
   );
 }
