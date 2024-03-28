ba11e0dd6902d74ceb7aa04b80db9dd3b7e62177
evan-rosa <evandanrosa@gmail.com>
2024-03-28 10:08:56 -0400

    add audiences back in layout

--- src/app/(dashboards)/dashboard/ga/properties/layout.tsx
+++ src/app/(dashboards)/dashboard/ga/properties/layout.tsx
@@ -15,6 +15,7 @@ export default async function PropertiesLayout({
   dimensions,
   metrics,
   conversions,
+  audiences,
 }: {
   children: React.ReactNode;
   streams: React.ReactNode;
@@ -22,6 +23,7 @@ export default async function PropertiesLayout({
   dimensions: React.ReactNode;
   metrics: React.ReactNode;
   conversions: React.ReactNode;
+  audiences: React.ReactNode;
 }) {
   const user = await currentUser();
   if (!user) return notFound();
@@ -41,7 +43,7 @@ export default async function PropertiesLayout({
         {dimensions}
         {metrics}
         {conversions}
-
+        {audiences}
       </div>
     </>
   );
